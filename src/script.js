af4190a100720e427e3af16adf4c7c4c10ddbc29
isaacklein080 <isaac.klein2022@gmail.com>
2025-11-04 14:59:24 -0800

    Delete src directory

--- src/script.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaWprbGVpbiIsImEiOiJjbWg5czN5aHQwYTA5MmxvajQxcHJkOGdqIn0.Hh8LHf_7JuYvAIVXWDyeXg';
-
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/ijklein/cmh9sad23000301sof7eo14do', //Your Style URL goes here
-  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9 // starting zoom
-    });
