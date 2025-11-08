4c86425e205128252ce3665da5bd1bcfa59ed7d2
isaacklein080 <isaac.klein2022@gmail.com>
2025-11-08 00:02:44 -0800

    Enhance map with controls and popup functionality
    Added navigation and scale controls to the map. Implemented click events for displaying popups with landmark information and added logic for a transparency panel.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaWprbGVpbiIsImEiOiJjbWg5czN5aHQwYTA5MmxvajQxcHJkOGdqIn0.Hh8LHf_7JuYvAIVXWDyeXg';
-
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/ijklein/cmh9sad23000301sof7eo14do', // Your Style URL
+  style: 'mapbox://styles/ijklein/cmh9sad23000301sof7eo14do',
   center: [-122.27, 37.87], // starting position [lng, lat]
   zoom: 9 // starting zoom
 });
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+map.addControl(new mapboxgl.ScaleControl({
+  maxWidth: 200,
+  unit: 'imperial' 
+}), 'bottom-left');
 
-map.on('load', function () {
+map.on('load', function() {
   map.addSource('points-data', {
     type: 'geojson',
     data: 'https://raw.githubusercontent.com/isaacklein080/BAHA-Map/refs/heads/main/data/183.geojson'
@@ -24,4 +28,48 @@ map.on('load', function () {
       'circle-stroke-color': '#ffffff'
     }
   });
+
+  map.on('click', 'points-layer', (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const properties = e.features[0].properties;
+
+    const popupContent = `
+      <div>
+        <h3>${properties["Landmark"]}</h3>
+        <p><strong>Address:</strong> ${properties["Address"]}</p>
+        <p><strong>Architect & Date:</strong> ${properties["Architect + Date"]}</p>
+        <p><strong>Designated:</strong> ${properties["Designated"]}</p>
+        ${properties["Link"] ? `<p><a href="${properties["Link"]}" target="_blank">More Information</a></p>` : ''}
+        ${properties["Notes"] ? `<p><strong>Notes:</strong> ${properties["Notes"]}</p>` : ''}
+      </div>
+    `;
+
+    new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(popupContent)
+      .addTo(map);
+  });
+
+    map.on('mouseenter', 'points-layer', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+ 
+    map.on('mouseleave', 'points-layer', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+});
+
+//Transparency Panel Logic
+const dataBtn = document.getElementById('data-info-btn');
+const dataPanel = document.getElementById('data-panel');
+const closeBtn = document.querySelector('.close-panel');
+
+dataBtn.addEventListener('click', function() {
+  dataPanel.classList.toggle('visible');
+});
+
+closeBtn.addEventListener('click', function() {
+  dataPanel.classList.remove('visible');
 });
